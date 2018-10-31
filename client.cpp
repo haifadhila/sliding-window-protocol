75413656d1f23a2f5b71fc538e1300a1d297c6ea
yulyulyharuka <yulyharuka@gmail.com>
2018-10-31 22:25:33 +0700

    fix segmentation fault bug

--- client.cpp
+++ client.cpp
@@ -1,13 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
 #include <strings.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -34,64 +35,76 @@ int readFile(string inputfile, string message){
 
 int main(int argc, char *argv[])
 {
+    char* filename;
+    char* ipaddr;
+    int windowSize, bufferSize, port;
+    int recvlen;
+
     if (argc != 6){
         cout << "wrong input" << endl;
         cout << "./sendfile filename windowsize buffersize IPaddress port" << endl;
         exit(0);
     }else{
-        filename = argv[1]
-        windowsize = atoi(argv[2]);
+        filename = argv[1];
+        windowSize = atoi(argv[2]);
         bufferSize = atoi(argv[3]);
-        ipaddr = argv[4]
+        ipaddr = argv[4];
         port = atoi(argv[5]);
     }
 
-
     int sockfd, n;
     struct sockaddr_in serv_addr, cli_addr;
-    char* server;
 
-    char buffer[1024];
-    
+    char* buffer;
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening socket");
-    server = ipaddr;
+
+    int slen = sizeof(serv_addr);
     // if (server == NULL) {
     //     cerr << "ERROR, no such host\n";
     //     exit(0);
     // }
     memset((char *) &cli_addr, 0, sizeof(cli_addr));
-    serv_addr.sin_family = AF_INET;
-    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    myaddr.sin_port = htons(0);
-    
-    if (bind(fd, (struct sockaddr *)&cli_addr, sizeof(cli_addr)) < 0) {
+    cli_addr.sin_family = AF_INET;
+    cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    cli_addr.sin_port = htons(0);
+
+    if (bind(sockfd, (struct sockaddr *)&cli_addr, sizeof(cli_addr)) < 0) {
         perror("bind failed");
         return 0;
     }
 
     memset((char *) &serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(server);
-    if (inet_aton(server, &serv_addr.sin_addr)==0) {
+    serv_addr.sin_port = htons(port);
+    if (inet_aton(ipaddr, &serv_addr.sin_addr)==0) {
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
     }
-    
-    for (i=0;i < 5;i++){
-        printf("Sending packet to %s port %d\n",server, port);
 
-    }
-    std::string buff(buffer);
-    readFile(filename,buffer);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    buffer = "hello";
+    printf("%s\n", buffer);
+    printf("%d\n", strlen(buffer));
+    // for (int i=0;i < 5;i++){
+        printf("Sending packet to %s port %d\n",ipaddr, port);
+        // readFile(filename,buffer);
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serv_addr, slen);
+
+        recvlen = recvfrom(sockfd, buffer, BUFFSIZE, 0, (struct sockaddr *)&serv_addr, &slen);
+          if (recvlen >= 0) {
+            buffer[recvlen] = 0;	/* expect a printable string - terminate it */
+            printf("received message: \"%s\"\n", buffer);
+        }
+	  // }
+    // n = write(sockfd,buffer,strlen(buffer));
+    // if (n < 0)
+    //      error("ERROR writing to socket");
+    //
+    // if (n < 0)
+    //      error("ERROR reading from socket");
+    // printf("%s\n",buffer);
     close(sockfd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -2,9 +2,10 @@
    The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
 #include <string>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <fstream>
@@ -28,76 +29,68 @@ int writeFile(char* message, char* outputfile){
     filename.close();
     return 0;
 }
- 
 
 int main(int argc, char *argv[])
 {
+    unsigned int windowSize;
+    char buffer[BUFFSIZE];
+    int bufferSize;
+    char* filename;
+    unsigned int port;
+    int msgcnt;
+
     if (argc != 5){
         cout << "input the right message." << endl;
         cout << "./recvfile filename windowsize buffersize port" << endl;
         exit(0);
     } else {
-		filename = argv[1];
-        windowsize = atoi(argv[2]);
+		    filename = argv[1];
+        windowSize = atoi(argv[2]);
         bufferSize = atoi(argv[3]);
         port = atoi(argv[4]);
     }
 
-     int sockfd, portno;
-     int windowSize;
+     int sockfd;
      socklen_t clilen;
-     char buffer[1024];
      struct sockaddr_in server_addr, cli_addr;
      int n;
      int recvlen;
-     
 
      // Create Socket
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
 
-    memset((char*)&server_addr, 0 ,sizeof(server_addr));
+    memset((char*)&server_addr,0,sizeof(server_addr));
 
-    //bind socke
+    //bind socket
 	 server_addr.sin_family = AF_INET;
 	 server_addr.sin_addr.s_addr = INADDR_ANY;
 	 server_addr.sin_port = htons(port);
 
      if (bind(sockfd, (struct sockaddr *) &server_addr,
-              sizeof(server_addr)) < 0) 
+              sizeof(server_addr)) < 0)
               error("Bind error");
-
-     listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     
 
-     for (;;) {
-		printf("waiting on port %d\n", SERVICE_PORT);
-		recvlen = recvfrom(sockfd, buffer, BUFFSIZE, 0, (struct sockaddr *)&cli_addr, &clilen);
-		if (recvlen > 0) {
-			buffer[recvlen] = 0;
-			printf("received message: \"%s\" (%d bytes)\n", buffer, recvlen);
-			writeFile(buffer,filename);
-		}
-		else
-			printf("uh oh - something went wrong!\n");
-		sprintf(buffer, "ack %d", msgcnt++);
-		printf("sending response \"%s\"\n", buffer);
-		if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&cli_addr, clilen) < 0)
-			perror("sendto");
+    for (;;) {
+		    printf("waiting on port %d\n", port);
+		    recvlen = recvfrom(sockfd, buffer, BUFFSIZE, 0, (struct sockaddr *)&cli_addr, &clilen);
+		    if (recvlen > 0) {
+			       buffer[recvlen] = 0;
+			       printf("received message: \"%s\" (%d bytes)\n", buffer, recvlen);
+			       writeFile(buffer,filename);
+		    }else{
+              printf("uh oh - something went wrong!\n");
+        }
+
+  		sprintf(buffer, "ack %d", msgcnt++);
+  		printf("sending response \"%s\"\n", buffer);
+  		if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&cli_addr, clilen) < 0)
+  			perror("sendto");
 	}
-     
 
-     
      printf("Here is the message: %s\n",buffer);
-     
-     close(newsockfd);
      close(sockfd);
-     return 0; 
+     return 0;
 }
